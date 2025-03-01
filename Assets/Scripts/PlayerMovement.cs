d3116e39cfc325447c61440c6b8283b40d85408b
Carson Bennett <carben2004@gmail.com>
2025-03-01 12:07:36 -0800

    player_moves_where_camera_points

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ public class PlayerMovement : MonoBehaviour
 {
     // Reference to Character Controller
     public CharacterController controller;
+    public Transform cam;
 
     public float speed = 6f;
 
@@ -20,10 +21,12 @@ public class PlayerMovement : MonoBehaviour
         if(direction.magnitude >= 0.1f)
         {
             
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothTurnVelocity, smoothTurn);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            controller.Move(direction * speed * Time.deltaTime);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
     }
 }
